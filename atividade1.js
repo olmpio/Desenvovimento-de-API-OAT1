0490b57a915478edff450fe35b1cbc9ff5dda7bb
Pedro Olímpio <pedroolimpio@gmail.com>
2026-09-06 00:56:18 -0300

    Atividade 1

---
+++ atividade1.js
@@ -0,0 +1,33 @@
+// Questão 1 (1,0 ponto) — Contador Regressivo para Datas Desenvolva uma função calcularContagemRegressiva(dataFutura) que recebe uma data futura (string no formato ISOou objeto Date) e retorna um objeto detalhando quantos dias, horas, minutos e segundos faltam até esse momento.
+
+
+function calcularContagemRegressiva(dataFutura){
+    const agora = new Date().getTime(); 
+
+    const futuro = new Date(dataFutura).getTime();  
+
+    const calculo =  futuro - agora; 
+
+    if(calculo <= 0 ) {
+        
+      texto: "A data já passou!" 
+    
+   };
+   const segundo = 1000;
+   const minuto = segundo * 60;
+   const hora = minuto * 60;
+   const dia = hora * 24;
+
+    const dias = Math.floor( calculo / dia) 
+   const horas = Math.floor((calculo % dia )/ hora)
+   const minutos = Math.floor((calculo % hora)/ minuto)
+   const segundos = Math.floor((calculo % minuto ) / segundo)
+
+   const texto = `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
+
+   return { dias, horas, minutos, segundos, texto} ; 
+
+  }
+
+const contagem = calcularContagemRegressiva('2016-06-09T00:28:00');
+console.log(contagem);
